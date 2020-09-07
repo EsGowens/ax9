6f28885709eb7ebb736f3b223aa76ab23dff7853
esgowens <kinggsone@gmail.com>
2020-09-07 15:18:11 -0400

    distortion setup

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+let font
+let graphic
+
+
+function preload() {
+font = loadFont("assets/spacegrotesk-medium.otf")
+}
+
+function setup () {
+createCanvas(600, 600)
+
+graphic = createGraphics(600, 600)
+
+graphic.fill("#f3c043")
+graphic.textFont(font)
+graphic.textSize(800)
+graphic.textAlign(CENTER, CENTER)
+graphic.text("a", 300, 240)
+}
+
+function draw () {
+    background("#e84e3c")
+
+    const tileSize = 50
+
+for (let x = 0; x < 12; x = x +1) {
+
+    for (let y = 0; y < 12; y = y +1) {
+        const distortion = sin(frameCount * 0.05) * 50
+        // source
+        const sx = x * tileSize + distortion
+        const sy = y * tileSize
+        const sw = tileSize
+        const sh = tileSize
+
+        // dest
+        const dx = x * tileSize
+        const dy = y * tileSize
+        const dw = tileSize
+        const dh = tileSize
+
+
+        image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+
+    }
+
+    
+    }
+}
